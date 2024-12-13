98d25fbf4876a77d8e348617eb5d347fe100f0f1
cloep87 <cloe2mz@gmail.com>
2024-12-13 09:58:56 +0100

    Update footer.php
    correction orthographe footer

--- site sae 301/footer.php
+++ site sae 301/footer.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="footer.css">
-    <title>Footer en forme de Chat</title>
+    <title>Footer</title>
 </head>
 <body>
     <footer>
 
-        <p>© 2024 Ami 4 Pattes. Tous droits réservés.</p>
+        <p>© 2024 Amis 4 Pattes. Tous droits réservés.</p>
     </footer>
 </body>
 </html>
